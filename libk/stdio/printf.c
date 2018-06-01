bd26369aede410e93113fe5cd6ddbdf4a3efc253
Wouter Schoenmakers <woutereldar@gmail.com>
2018-06-01 21:43:07 +0200

    Worked on improvements of printf

--- libk/stdio/printf.c
+++ libk/stdio/printf.c
@@ -4,7 +4,37 @@
 #include <stdio.h>
 #include <string.h>
 
-char *convert(unsigned int num, int base);
+/**
+ * Converts a number to its equivalent with base 'base'
+ * @param num: the integer to be converted
+ * @param base: the base for the converted number
+ * @return: a pointer to a char array with the new number
+ */
+char *convert(int num, int base) {
+    static char Representation[]= "0123456789ABCDEF";
+    static char buffer[128];
+    char *ptr;
+
+    unsigned int a = num;
+
+    ptr = &buffer[127];
+    *ptr = '\0';
+
+    if(base < 2 || base > 16){
+    	return ptr;
+    }
+
+    if(num < 0){
+    	a = -1*num;
+    }
+
+    do {
+    	*--ptr = Representation[a%base];
+        a /= base;
+    } while(a != 0);
+
+    return(ptr);
+}
 
 static int print(const char* data, size_t length) {
 	const unsigned char* bytes = (const unsigned char*) data;
@@ -20,24 +50,49 @@ int printf(const char* format, ...) {
 
 	int written = 0;
 
-	while (*format != '\0') {
-		size_t maxrem = INT_MAX - written;
+	while(*format != '\0'){
+		size_t max_remaining = INT_MAX - written;
 
 		if (format[0] != '%' || format[1] == '%') {
-			if (format[0] == '%')
+			if (format[0] == '%'){
 				format++;
-			size_t amount = 1;
-			while (format[amount] && format[amount] != '%')
-				amount++;
-			if (maxrem < amount) {
+			}
+
+			size_t format_len = 1;
+
+			while (format[format_len] && format[format_len] != '%'){
+				format_len++;
+			}
+
+			if (max_remaining < format_len) {
 				// TODO: Set errno to EOVERFLOW.
 				return -1;
 			}
-			if (!print(format, amount))
+
+			if (!print(format, format_len)){
 				return -1;
-			format += amount;
-			written += amount;
+			}
+
+			format += format_len;
+			written += format_len;
+
 			continue;
+		}
+
+		int zero_padding = 0;
+		int prefix_enable = 0;
+
+		//TODO: Finish implementation
+		if(*format == '#'){
+			prefix_enable = 1;
+			format++;
+		} else if(*format == '0'){
+			zero_padding = 1;
+			format++;
+		}
+
+		if(*format >= '0' && *format <= '9'){
+			//TODO: Implement
 		}
 
 		const char* format_begun_at = format++;
@@ -45,7 +100,7 @@ int printf(const char* format, ...) {
 		if (*format == 'c') {
 			format++;
 			char c = (char) va_arg(parameters, int /* char promotes to int */);
-			if (!maxrem) {
+			if (!max_remaining) {
 				// TODO: Set errno to EOVERFLOW.
 				return -1;
 			}
@@ -56,7 +111,7 @@ int printf(const char* format, ...) {
 			format++;
 			const char* str = va_arg(parameters, const char*);
 			size_t len = strlen(str);
-			if (maxrem < len) {
+			if (max_remaining < len) {
 				// TODO: Set errno to EOVERFLOW.
 				return -1;
 			}
@@ -68,7 +123,7 @@ int printf(const char* format, ...) {
 			unsigned int hex = va_arg(parameters, unsigned int);
 			const char *str = convert(hex, 16);
 			size_t len = strlen(str);
-			if (maxrem < len) {
+			if (max_remaining < len) {
 				// TODO: Set errno to EOVERFLOW.
 				return -1;
 			}
@@ -82,7 +137,7 @@ int printf(const char* format, ...) {
 			unsigned int dec = va_arg(parameters, unsigned int);
 			const char *str = convert(dec, 10);
 			size_t len = strlen(str);
-			if (maxrem < len) {
+			if (max_remaining < len) {
 				// TODO: Set errno to EOVERFLOW.
 				return -1;
 			}
@@ -96,7 +151,7 @@ int printf(const char* format, ...) {
 			unsigned int bin = va_arg(parameters, unsigned int);
 			const char *str = convert(bin, 2);
 			size_t len = strlen(str);
-			if (maxrem < len) {
+			if (max_remaining < len) {
 				// TODO: Set errno to EOVERFLOW.
 				return -1;
 			}
@@ -108,7 +163,7 @@ int printf(const char* format, ...) {
 		} else {
 			format = format_begun_at;
 			size_t len = strlen(format);
-			if (maxrem < len) {
+			if (max_remaining < len) {
 				// TODO: Set errno to EOVERFLOW.
 				return -1;
 			}
@@ -117,30 +172,9 @@ int printf(const char* format, ...) {
 			written += len;
 			format += len;
 		}
+
 	}
 
 	va_end(parameters);
 	return written;
-}
-
-/**
- * Converts a number to its equivalent with base 'base'
- * @param num: the integer to be converted
- * @param base: the base for the converted number
- * @return: a pointer to a char array with the new number
- */
-char *convert(unsigned int num, int base) {
-    static char Representation[]= "0123456789ABCDEF";
-    static char buffer[50];
-    char *ptr;
-
-    ptr = &buffer[49];
-    *ptr = '\0';
-
-    do {
-    	*--ptr = Representation[num%base];
-        num /= base;
-    } while(num != 0);
-
-    return(ptr);
 }
